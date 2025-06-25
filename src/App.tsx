cc17f104322c549791d0aff1960627fcaf10e735
Arthur Gustavo <artgustavobarros@gmail.com>
2025-06-25 01:51:16 -0300

    menu section

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Hero } from "./components/hero";
 import { Cocktails } from "./components/cocktails";
 import { About } from "./components/about";
 import { Art } from "./components/art";
+import { Menu } from "./components/menu,";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -16,6 +17,7 @@ export function App() {
       <Cocktails />
       <About />
       <Art />
+      <Menu />
     </main>
   );
 }
