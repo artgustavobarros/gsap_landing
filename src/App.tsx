c5298fe1db9932d67e71879e601aa206fbad606b
Arthur Gustavo <artgustavobarros@gmail.com>
2025-06-24 21:14:53 -0300

    hero layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import { Navbar } from "./components/navbar";
+import { Hero } from "./components/hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export function App() {
   return (
-    <div className="flex-center h-[100vh]">
-      <div className="text-3xl text-indigo-300">Hello, GSAP!</div>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black" />
+    </main>
   );
 }
